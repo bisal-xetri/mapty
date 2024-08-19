8bb5753cf2ec83535232dc8640907e703b308b27
bisal-xetri <bishaldkl56@gmail.com>
2024-08-19 23:00:50 +0545

    Display map marker

--- script.js
+++ script.js
@@ -11,28 +11,49 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-//who want to why 
+//who want to why
 
-if(navigator.geolocation){
-navigator.geolocation.getCurrentPosition(function(position){
- const {latitude}=position.coords;
- const {longitude}=position.coords;
- console.log(latitude,longitude)
- console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
- const coords=[latitude,longitude];
- 
- const map = L.map('map').setView(coords, 13);
+      const coords = [latitude, longitude];
 
-L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+      const map = L.map('map').setView(coords, 13);
+      console.log(map)
 
-L.marker(coords).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
 
-}, function(){
-    alert('Could not get your position.')
-})
-};
+     
+
+
+        map.on('click',function(mapEvent){
+            console.log(mapEvent)
+            const {lat, lng}=mapEvent.latlng;
+            L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup(L.popup({
+                maxWidth:250,
+                minWidth:100,
+                autoClose:false,
+                closeOnClick:false,
+                className:'running-popup',
+                
+            }))
+            .setPopupContent('workout')
+            .openPopup();
+        })//leaflet library 
+
+    },
+    function () {
+      alert('Could not get your position.');
+    }
+  );
+}
